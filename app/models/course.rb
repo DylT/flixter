a17e787894707fb5c0399f678c5592a6409a2f4f
Dylan <dyltrz@gmail.com>
2019-08-23 01:50:52 +0000

    Added carrierwave gem and created ed uploader.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,8 @@ class Course < ApplicationRecord
   belongs_to :user
   has_many :sections
 
+  mount_uploader :image, ImageUploader
+
   validates :title, :description, :cost, presence: true
   validates :cost, numericality: {greater_than_or_equal_to: 0}
 end
